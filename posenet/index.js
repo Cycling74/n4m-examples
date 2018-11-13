61a92d73bc67a2437c8ace31fd69bbb5b16295d4
Yuichi Yogo <yuichkun@gmail.com>
2018-11-13 21:08:43 +0900

    fix eslint

--- posenet/index.js
+++ posenet/index.js
@@ -2,20 +2,20 @@
 // When this file is executed via `node.script start` in the Max patch,
 // this program will launch an Electron app as a child process, and connect to it by socket.io
 
-const MaxAPI = require('max-api');
-const io = require('socket.io')();
-const electron = require('electron');
-const proc = require('child_process');
-const child = proc.spawn(electron, ['.']);
+const MaxAPI = require("max-api");
+const io = require("socket.io")();
+const electron = require("electron");
+const proc = require("child_process");
+const child = proc.spawn(electron, ["."]);
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
 
-  console.log('Socket is connected with Electron App');
+	console.log("Socket is connected with Electron App");
 
-  socket.on('dispatch', (data) => { 
-    console.log('dispatch: ', data);
-    MaxAPI.outlet(data);
-  });
+	socket.on("dispatch", (data) => {
+		console.log("dispatch: ", data);
+		MaxAPI.outlet(data);
+	});
 
 });
 
@@ -23,6 +23,6 @@ io.listen(3000);
 
 // This will ensure that when this parent process is killed in maxpat (either by `node.script stop` or Max is shutdown for some reason),
 // it will terminate the child process, the Electron app.
-process.on('exit', () => {
-  child.kill();
+process.on("exit", () => {
+	child.kill();
 });
--- posenet/main.js
+++ posenet/main.js
@@ -1,12 +1,12 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
-function createWindow () {
-  console.log('hello');
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+function createWindow() {
+	console.log("hello");
+	// Create the browser window.
+	const win = new BrowserWindow({ width: 800, height: 600 });
 
-  // and load the html of the app.
-  win.loadFile('./camera.html');
+	// and load the html of the app.
+	win.loadFile("./camera.html");
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
