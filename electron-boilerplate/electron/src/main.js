0fcecb7906f3df47e7adb6a1192079e29ee1a0d9
Yuichi Yogo <yuichkun@gmail.com>
2018-12-08 01:31:46 +0900

    eslint fix

--- electron-boilerplate/electron/src/main.js
+++ electron-boilerplate/electron/src/main.js
@@ -3,34 +3,35 @@
 // this file will be the entry point for Electron App
 // when `npm start` is called.
 
-const { app, BrowserWindow } = require('electron');
 
-app.on('ready', () => {
+const { app, BrowserWindow } = require("electron");
 
-  console.log('Electron has started');
+app.on("ready", () => {
 
-  // Launch a window and load index.html
-  let window = new BrowserWindow({width: 450, height: 800});
-  window.loadFile('./src/index.html');
+	console.log("Electron has started");
 
-  window.on('closed', () => {
-    window = null;
-  });
+	// Launch a window and load index.html
+	let window = new BrowserWindow({width: 450, height: 800});
+	window.loadFile("./src/index.html");
 
-  // For Development Only
-  if (process.env.DEBUG) {
-    // This will not be open when it's called in Max patch,
-    // because the environment variable DEBUG will not be provided.
-    // c.f. See ../../index.js, where it calls child_process.spawn to invoke Electron
-    window.webContents.openDevTools();
-  }
+	window.on("closed", () => {
+		window = null;
+	});
+
+	// For Development Only
+	if (process.env.DEBUG) {
+		// This will not be open when it's called in Max patch,
+		// because the environment variable DEBUG will not be provided.
+		// c.f. See ../../index.js, where it calls child_process.spawn to invoke Electron
+		window.webContents.openDevTools();
+	}
 
 });
 
-app.on('window-all-closed', function() {
+app.on("window-all-closed", function () {
 
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 
 });
--- electron-boilerplate/electron/webpack.config.js
+++ electron-boilerplate/electron/webpack.config.js
@@ -1,14 +1,16 @@
+
+
 module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
+	entry: "./src/index.js",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
 };
