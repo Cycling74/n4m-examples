bfead0d7bb61baff16bbb94f0fa5e257a8e01556
Contra <dadadaluo@gmail.com>
2019-01-21 13:57:22 +0800

    Strings must use doublequote .

--- qrcode-generator/qrcode-generator.js
+++ qrcode-generator/qrcode-generator.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const maxAPI = require("max-api");
-const QRCode = require('qrcode');
+const QRCode = require("qrcode");
 
 maxAPI.addHandler("genQR", (...args) => {
-	console.log('genQR', args);
-	const text = args[0];//here only parse first param received.
+	console.log("genQR", args);
+	const text = args[0]; // here only parse first param received.
 	const fileName = Date.now();
-	const filePath = __dirname + '/qrcode/' + fileName + '.png';
+	const filePath = __dirname + "/qrcode/" + fileName + ".png";
 	QRCode.toFile(filePath, text, {
 		// color: {
 		// 	dark: '#00F',  // Blue dots
@@ -15,7 +15,7 @@ maxAPI.addHandler("genQR", (...args) => {
 		// }
 	}, function (err) {
 		if (err) throw err;
-		console.log('qr file done');
+		console.log("qr file done");
 		// maxAPI.post("fileDone", filePath);
 		maxAPI.outlet("filePath", filePath);
 	});
